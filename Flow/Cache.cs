86ff88ebe9aa6ee92f7b7fae45b247f1fe9482a5
ERufian <github@rufian.zilbermann.com>
2014-11-06 14:07:23 -0500

    2-Tier LRU streaming cache

---
+++ Flow/Cache.cs
@@ -0,0 +1,135 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Cache.cs" company="Eusebio Rufian-Zilbermann">
+//   Copyright (c) Eusebio Rufian-Zilbermann
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Ticker
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+
+   /// <summary>
+   /// Two level Least Recently Used cache for a stream of stock quotes.
+   /// </summary>
+   public class Cache
+   {
+      /// <summary>
+      /// Maximum Number of Securities to cache.
+      /// </summary>
+      private readonly int numSecurities;
+
+      /// <summary>
+      /// Maximum Number of prices to cache for each security.
+      /// </summary>
+      private readonly int numPrices;
+
+      /// <summary>
+      /// List of last N accessed securities, ordered by last access.
+      /// </summary>
+      private LinkedList<string> lastN;
+
+      /// <summary>
+      /// Relation of security names to price lists.
+      /// </summary>
+      private Dictionary<string, int> currentSymbols;
+
+      /// <summary>
+      /// Last N prices received for a specific security.
+      /// </summary>
+      private Queue<decimal>[] prices;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="Cache"/> class.
+      /// </summary>
+      /// <param name="numSecurities">The maximum number of securities to cache.</param>
+      /// <param name="numPrices">The maximum number of prices to cache for each security.</param>
+      public Cache(int numSecurities, int numPrices)
+      {
+         this.numSecurities = numSecurities;
+         this.numPrices = numPrices;
+         this.lastN = new LinkedList<string>();
+         this.currentSymbols = new Dictionary<string, int>(this.numSecurities);
+         this.prices = new Queue<decimal>[this.numSecurities];
+         this.InitializeWithDummyData();
+      }
+
+      /// <summary>
+      /// Cache information for the latest quote tick.
+      /// </summary>
+      /// <param name="security">Name of the security in the quote tick.</param>
+      /// <param name="price">Price of the security in the quote tick.</param>
+      public void Tick(string security, decimal price)
+      {
+         if (this.currentSymbols.ContainsKey(security))
+         {
+            if (0 != string.Compare(this.lastN.Last.Value, security))
+            {
+               this.lastN.Remove(security); // This is potentially slow (comparatively), O(numSecurities)
+               this.lastN.AddLast(security);
+            }
+
+            if (this.prices[this.currentSymbols[security]].Count >= this.numPrices)
+            {
+               this.prices[this.currentSymbols[security]].Dequeue();
+            }
+
+            this.prices[this.currentSymbols[security]].Enqueue(price);
+         }
+         else
+         {
+            string removedSecurity = this.lastN.First.Value;
+            int removedIndex = this.currentSymbols[removedSecurity];
+            this.lastN.RemoveFirst();
+            this.lastN.AddLast(security);
+            this.prices[removedIndex] = new Queue<decimal>();
+            this.prices[removedIndex].Enqueue(price);
+            this.currentSymbols.Remove(removedSecurity);
+            this.currentSymbols.Add(security, removedIndex);
+         }
+      }
+
+      /// <summary>
+      /// Get the latest N prices in the cache for a given security.
+      /// </summary>
+      /// <param name="security">The security to search for.</param>
+      /// <returns>An enumeration of prices, or null if the security is not found in the cache.</returns>
+      public IEnumerable<decimal> LatestNPrices(string security)
+      {
+         return this.currentSymbols.ContainsKey(security) ?
+            this.prices[this.currentSymbols[security]].AsEnumerable() :
+            (IEnumerable<decimal>)null;
+      }
+
+      /// <summary>
+      /// Initialize the cache with data to be discarded when actual ticks come in.
+      /// </summary>
+      /// <remarks>
+      /// It is also possible to initialize the cache as empty and then fill it up
+      /// as items come in, that would make instantiation faster and initial memory consumption lower
+      /// but that would require additional checks during normal operation and different behavior
+      /// when the cache is in a partially primed state. If the expectation is for a long-running
+      /// cache where number of ticks is much greater number of securities cached, it is more effective
+      /// overall pre-populating the cache with data.
+      /// </remarks>
+      private void InitializeWithDummyData()
+      {
+         for (int i = 0; this.numSecurities > i; i++)
+         {
+            this.lastN.AddLast("Dummy" + i);
+         }
+
+         for (int i = 0; this.numSecurities > i; i++)
+         {
+            this.currentSymbols.Add("Dummy" + i, i);
+         }
+
+         for (int i = 0; this.numSecurities > i; i++)
+         {
+            this.prices[i] = new Queue<decimal>(this.numPrices);
+         }
+      }
+   }
+}
---
+++ FlowUnitTests/CacheTests.cs
@@ -0,0 +1,168 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CacheTests.cs" company="Eusebio Rufian-Zilbermann">
+//   Copyright (c) Eusebio Rufian-Zilbermann
+// </copyright>
+//-----------------------------------------------------------------------
+namespace FlowUnitTests
+{
+   using System;
+   using System.Collections;
+   using System.Collections.Generic;
+
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   /// <summary>
+   /// Unit tests for the Cache class.
+   /// </summary>
+   [TestClass]
+   public class CacheTests
+   {
+      /// <summary>
+      /// Simple case, insert one tick.
+      /// </summary>
+      [TestMethod]
+      public void Insert1Security()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 200 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         Assert.IsNull(latestNPrices);
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert 2 ticks, with different securities each.
+      /// </summary>
+      [TestMethod]
+      public void Insert2DifferentSecurities()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 200 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         CollectionAssert.AreEqual(new List<decimal> { 75 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert multiple ticks, with one security getting 2 prices.
+      /// </summary>
+      [TestMethod]
+      public void Insert2PricesFor1Security()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         cache.Tick("IBM", 201);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 200, 201 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         CollectionAssert.AreEqual(new List<decimal> { 75 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert multiple ticks, with two securities getting 2 prices.
+      /// </summary>
+      [TestMethod]
+      public void Insert2PricesFor2Securities()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         cache.Tick("IBM", 201);
+         cache.Tick("FB", 76);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 200, 201 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         CollectionAssert.AreEqual(new List<decimal> { 75, 76 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert many ticks, enough to get one old price dropped from the cache.
+      /// </summary>
+      [TestMethod]
+      public void DropOldPrice()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         cache.Tick("IBM", 201);
+         cache.Tick("FB", 76);
+         cache.Tick("IBM", 202);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 201, 202 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         CollectionAssert.AreEqual(new List<decimal> { 75, 76 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert many ticks, with two consecutive ticks for the same security,
+      /// to test the skip-move-to-the-end optimization.
+      /// </summary>
+      [TestMethod]
+      public void ConsecutiveTicksForSameSecurity()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         cache.Tick("IBM", 201);
+         cache.Tick("FB", 76);
+         cache.Tick("IBM", 202);
+         cache.Tick("IBM", 203);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 202, 203 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         CollectionAssert.AreEqual(new List<decimal> { 75, 76 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("MSFT");
+         Assert.IsNull(latestNPrices);
+      }
+
+      /// <summary>
+      /// Insert many ticks, enough to get one old security dropped from the cache.
+      /// </summary>
+      [TestMethod]
+      public void DropOldSecurity()
+      {
+         IEnumerable<decimal> latestNPrices;
+
+         Ticker.Cache cache = new Ticker.Cache(2, 2);
+         cache.Tick("IBM", 200);
+         cache.Tick("FB", 75);
+         cache.Tick("IBM", 201);
+         cache.Tick("FB", 76);
+         cache.Tick("IBM", 202);
+         cache.Tick("IBM", 203);
+         cache.Tick("MSFT", 50);
+         latestNPrices = cache.LatestNPrices("IBM");
+         CollectionAssert.AreEqual(new List<decimal> { 202, 203 }, new List<decimal>(latestNPrices));
+         latestNPrices = cache.LatestNPrices("FB");
+         Assert.IsNull(latestNPrices);
+         latestNPrices = cache.LatestNPrices("MSFT");
+         CollectionAssert.AreEqual(new List<decimal> { 50 }, new List<decimal>(latestNPrices));
+      }
+   }
+}
